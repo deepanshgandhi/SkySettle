57313a5ad44b68a08bc39b6d656126aec6245657
Gibran <myageri.g@northeastern.edu>
2025-04-15 18:28:55 -0400

    added cors middleware

--- flight_assistant/main.py
+++ flight_assistant/main.py
@@ -10,9 +10,20 @@ import time
 from flight_assistant.policy_loader import load_policies
 from flight_assistant.lm import call_language_model
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
 
 app = FastAPI()
+
+# add CORS middleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8081",],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
 RAPIDAPI_HOST = "aerodatabox.p.rapidapi.com"
